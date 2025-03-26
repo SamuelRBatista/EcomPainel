addf695c9f57820f98a7aa0a92c546f5adb478be
SamuelRBatista <samuel.r.batista1983@gmail.com>
2025-03-26 09:20:52 -0300

    feat:26032025 => Inclusão do Layout, js e estilos

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.left = sidebar.style.left === '0px' ? '-300px' : '0px';
+}
 
-// Write your JavaScript code.
+
